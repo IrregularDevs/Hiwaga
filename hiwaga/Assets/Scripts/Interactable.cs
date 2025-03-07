aee9ec3c96018cdcebe7dc95ac6cf9307f77fd1e
xJabee <jabee.sevilla@gmail.com>
2025-03-07 18:49:34 +0800

    Added function for multiple dialogues per character

--- hiwaga/Assets/Scripts/Interactable.cs
+++ hiwaga/Assets/Scripts/Interactable.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class Interactable : MonoBehaviour
 {
     [SerializeField]
     private string enterString, exitString, interactString;
     [SerializeField]
-    private DialogueManager dialogueManager;
+    private List<DialogueManager> dialogueManagers; // List of DialogueManager
     [SerializeField]
     private TextMeshProUGUI dialogueText, nameText;
     [SerializeField]
     private GameObject panel;
 
+    private int currentDialogueIndex = 0;
+
     public void enterPrompt()
     {
-        Debug.Log($"Hi I'm {dialogueManager.character_name} nice to meet you");
+        Debug.Log($"Hi I'm {dialogueManagers[currentDialogueIndex].character_name} nice to meet you");
         //Show an indicator to the player that they can interact with the object
     }
 
@@ -27,14 +30,29 @@ public class Interactable : MonoBehaviour
 
     public void interactPrompt()
     {
-        //Show the dialogue box
-        panel.SetActive(true);
-        //Show the name of the character
-        nameText.text = dialogueManager.character_name;
-        Debug.Log(dialogueManager.dialogue);
-        //Make the UI Show the Text Corresponding to the character
-        dialogueText.text = dialogueManager.dialogue;
+        if (currentDialogueIndex < dialogueManagers.Count)
+        {
+            //Show the dialogue box
+            panel.SetActive(true);
+            //Show the name of the character
+            nameText.text = dialogueManagers[currentDialogueIndex].character_name;
+            Debug.Log(dialogueManagers[currentDialogueIndex].dialogue);
+            //Make the UI Show the Text Corresponding to the character
+            dialogueText.text = dialogueManagers[currentDialogueIndex].dialogue;
 
+            // Move to the next dialogue
+            currentDialogueIndex++;
+        }
+        else
+        {
+            // No more dialogues, hide the panel
+            panel.SetActive(false);
+            currentDialogueIndex = 0; // Reset the index if needed
+        }
+    }
 
+    private void interaction()
+    {
+        //This is where the interaction happens
     }
 }
