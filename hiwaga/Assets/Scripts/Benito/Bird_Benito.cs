24e28f5a1bda5183b0b31a0552220c9cdcf24dce
Chef-Boyardeee <ericcartmanwizardking@gmail.com>
2026-07-04 06:38:11 +0800

    Benito Bird Bug Fix

--- hiwaga/Assets/Scripts/Benito/Bird_Benito.cs
+++ hiwaga/Assets/Scripts/Benito/Bird_Benito.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Bird_Benito : NPC
 {
     [SerializeField] private NPC_ModelChanger modelChanger;
+    private bool hasTalked = false;
 
     protected override void Start()
     {
@@ -19,5 +20,15 @@ public class Bird_Benito : NPC
     {
         modelChanger.ChangeModel(1);
         onBeginDialogue -= ChangeModel;
+    }
+
+    public override void Interact()
+    {
+        if(!hasTalked)
+        {
+            DialogueManager.Instance.UpdateDialogue(GetRefName(), 1);
+            hasTalked = true;
+        }
+        base.Interact();
     }
 }
